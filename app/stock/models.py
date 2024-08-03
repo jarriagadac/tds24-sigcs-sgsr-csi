e66776e2781fc6a012ab97d3971f466b8de4cd14
francisco navarro <f.nav.del@gmail.com>
2024-08-03 12:41:42 -0400

    U-001 modificacion de modelo pase los test

--- app/stock/models.py
+++ app/stock/models.py
@@ -8,17 +8,18 @@ class Lote(models.Model):
     medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField()
     fecha_vencimiento = models.DateField()
+    has_transfer = models.BooleanField(default=False)
+    vencido = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"{self.codigo}"
-        #return f"{self.codigo} ({self.cantidad}) - vence: {self.fecha_vencimiento}" #Comentado por error en test_basicos test_lote_model
 
 
 class Consumo(models.Model):
     institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
     medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
-    cantidad = models.PositiveIntegerField()
-    fecha = models.DateField(default=date.today)
+    cantidad = models.PositiveIntegerField(default=0)
+    fecha = models.DateField(auto_now_add=True)
 
     def __str__(self) -> str:
         return f"{self.institucion} - {self.medicamento} ({self.cantidad}) - fecha: {self.fecha}"
@@ -28,7 +29,7 @@ class Stock(models.Model):
     institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
     medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField(default=0)
-    fecha_actualizacion = models.DateField(default=date.today)
+    fecha_actualizacion = models.DateField(auto_now_add=True)
     has_quiebre = models.BooleanField(default=False)
 
     class Meta:
@@ -40,13 +41,8 @@ class Stock(models.Model):
 
 class Movimiento(models.Model):
     institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
-    lote = models.ForeignKey(Lote, on_delete=models.CASCADE)
-    fecha = models.DateField(default=date.today)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['lote'], name='unique_lote')
-        ]
+    lote = models.OneToOneField(Lote, on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now_add=True)
 
     def __str__(self) -> str:
         return f"{self.institucion} - {self.lote.codigo} ({self.lote.cantidad}) - fecha: {self.fecha}"

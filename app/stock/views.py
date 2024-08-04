e1831166ad827e1c847937b3b7064b7ed7cf457c
manunezg <miguel.nunez.gajardo@gmail.com>
2024-08-03 20:18:00 -0400

    Avance en Troubleshooting de errores.
    Avance en Troubleshooting de errores.

--- app/stock/views.py
+++ app/stock/views.py
@@ -1,23 +1,39 @@
-class MovimientoListCreateView:
+from django.views.generic import ListView, CreateView, RetrieveUpdateDestroyAPIView
+from .models import Movimiento
+
+# Vista para listar y crear movimientos
+class MovimientoListCreateView(ListView, CreateView):
+    model = Movimiento
+    template_name = 'movimiento_list.html'
+    context_object_name = 'movimientos'
+    fields = '__all__'
+
+# Vista para recuperar y destruir movimientos
+class MovimientoRetrieveDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Movimiento.objects.all()
+    serializer_class = MovimientoSerializer  # Asegúrate de tener un serializer
+
+# Otras vistas (proporciona una herencia adecuada y evita definir as_view manualmente)
+class ConsumoMedicamentoAPIView(ListView, CreateView):
     pass
 
-
-class MovimientoRetrieveDestroyView:
+class DisponibilidadMedicamentoAPIView(ListView, CreateView):
     pass
 
-
-class MovimientoLoteRetrieveView:
+class QuiebreStockAPIView(ListView, CreateView):
     pass
 
-
-class MovimientoMedicamentoView:
-    def get():
-        pass
-
-
-class ConsumoListCreateView:
+class AlertaCaducidadLoteAPIView(ListView, CreateView):
     pass
 
+class MovimientoLoteRetrieveView(ListView, CreateView):
+    pass
 
-class ConsumoRetrieveDestroyView:
+class MovimientoMedicamentoView(ListView, CreateView):
+    pass
+
+class ConsumoListCreateView(ListView, CreateView):
+    pass
+
+class ConsumoRetrieveDestroyView(ListView, CreateView):
     pass
